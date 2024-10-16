0a67eb28d91145dbf5dcc0118bc0cbada4b77434
Chichari2 <100235931+Chichari2@users.noreply.github.com>
2024-10-16 17:48:06 +0200

    changed files for deployment

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/site.db'  # или другой URI
+    # Замените на URI вашей базы данных PostgreSQL
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///instance/site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'your_jwt_secret_key' 
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your_jwt_secret_key')  # Вы можете установить секретный ключ через переменные окружения
